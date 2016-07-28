0ebb65ff0a9061823f78ac26cfeffdb6508bc496
mid120 <1054378935@qq.com>
2016-07-28 09:46:11 +0800

    代码提交

--- src/containerStudy/ListExtends.scala
+++ src/containerStudy/ListExtends.scala
@@ -6,21 +6,21 @@ package containerStudy
  */
 
 object ListExtends {
-def main(args: Array[String]): Unit = {
- val c =new Cow()
- val m =new Mon()
-// c^m
-// c^:m
-   +c
-}
+  def main(args: Array[String]): Unit = {
+    val c = new Cow()
+    val m = new Mon()
+    // c^m
+    // c^:m
+    +c
+  }
 }
 
-class Cow{
+class Cow {
   //注意等号中间的空格
-	def unary_+ = println("------+号映射的方法")
-  def ^(m:Mon)={println("cow的方法")}
-  
+  def unary_+ = println("------+号映射的方法")
+  def ^(m: Mon) = { println("cow的方法") }
+
 }
-class Mon{
-  def ^:(c:Cow)={println("Mon的方法")}
+class Mon {
+  def ^:(c: Cow) = { println("Mon的方法") }
 }
